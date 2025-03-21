e378160e97117a92f10851b91322ea20d74a72c8
cyrius111 <148023375+cyrius111@users.noreply.github.com>
2025-03-21 12:28:46 -0500

    Create inicio.js

---
+++ inicio.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const startBtn = document.getElementById("startBtn");
+    if (startBtn) {
+        startBtn.addEventListener("click", function () {
+            window.location.href = "carta.html";
+        });
+    }
+});
